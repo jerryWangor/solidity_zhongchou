f4f194be53832e312e9defcb1b3f8a9d5da23b8d
Jerry <warjary@qq.com>
2022-02-25 11:06:36 +0800

    code1

--- contracts/ZhongChou.sol
+++ contracts/ZhongChou.sol
@@ -14,7 +14,7 @@ contract ZhongChou {
         // 账户金额（已经筹集的）
         uint amount;
         // 捐款人个数
-        uint funderCount;
+        uint8 funderCount;
         // 捐款人地址与捐款人详细信息映射
         mapping(address => Funder) funderMap;
         // 是否存在
@@ -31,8 +31,8 @@ contract ZhongChou {
         // 捐款人其他的一些信息
     }
 
-    uint neederId;
-    mapping(uint => Needer) neederMap;
+    uint8 neederId;
+    mapping(uint8 => Needer) neederMap;
 
     // 构造函数
     constructor() {
@@ -43,7 +43,7 @@ contract ZhongChou {
      * 这里理论上只能合约创始人才能创建众筹
      * 会返回一个众筹ID，捐款的时候传哪个ID就给哪个人捐
      */
-    function CreateNeeder(address _addr, uint _goal) public returns(uint, uint, bool, address) {
+    function CreateNeeder(address _addr, uint _goal) public returns(uint8, uint, bool, address) {
         require(msg.sender == owner, "not solidity creator");
         neederId++;
         neederMap[neederId].neederAddress = _addr;
@@ -56,37 +56,37 @@ contract ZhongChou {
     /**
      * 捐献money
      */
-    function Contribute(uint _neederId) public payable {
+    function Contribute(uint8 _neederId) public payable {
         // 判断有没有这个众筹数据
         require(neederMap[_neederId].isExists, "needer not exists");
-        neederMap[neederId].amount += msg.value;
+        neederMap[_neederId].amount += msg.value;
 
         // 判断这个是之前是否捐献过了
-        if(!neederMap[neederId].funderMap[msg.sender].isExists) {
-            neederMap[neederId].funderMap[msg.sender].isExists = true;
-            neederMap[neederId].funderMap[msg.sender].funderAddress = address(msg.sender);
-            neederMap[neederId].funderMap[msg.sender].funderMoney = msg.value;
+        if(!neederMap[_neederId].funderMap[msg.sender].isExists) {
+            neederMap[_neederId].funderMap[msg.sender].isExists = true;
+            neederMap[_neederId].funderMap[msg.sender].funderAddress = address(msg.sender);
+            neederMap[_neederId].funderMap[msg.sender].funderMoney = msg.value;
         } else {
-            neederMap[neederId].funderMap[msg.sender].funderMoney += msg.value;
+            neederMap[_neederId].funderMap[msg.sender].funderMoney += msg.value;
         }
     }
 
     // 把某个众筹项目下的全部钱转移给某个人，必须是合约创建人才能操作
-    function TransferNeederAmount(uint _neederId) public payable {
+    function TransferNeederAmount(uint8 _neederId) public payable {
         require(msg.sender == owner, "not solidity creator");
         require(neederMap[_neederId].isExists, "needer not exists");
         // 判断是否大于等于目标筹款金额，筹集够了就直接打款到受益人地址
-        if(neederMap[neederId].amount >= neederMap[neederId].goal) {
-            payable(neederMap[neederId].neederAddress).transfer(neederMap[neederId].amount);
+        if(neederMap[_neederId].amount >= neederMap[_neederId].goal) {
+            payable(neederMap[_neederId].neederAddress).transfer(neederMap[_neederId].amount);
         } else {
             revert("money not enough");
         }
     }
 
     // 获取某个众筹项目下的众筹总金额，当前筹集到的金额
-    function GetNeederContent(uint _neederId) public view returns(uint, uint) {
+    function GetNeederContent(uint8 _neederId) public view returns(uint, uint) {
         require(neederMap[_neederId].isExists, "needer not exists");
-        return (neederMap[neederId].goal, neederMap[neederId].amount);
+        return (neederMap[_neederId].goal, neederMap[_neederId].amount);
     }
 
     
